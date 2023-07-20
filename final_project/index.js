61955898c76389bccb69c8a5d2b16d22f05de9cc
Francisco Delgado L <franciscodelgalop@gmail.com>
2023-07-20 16:44:49 -0400

    changes made

--- final_project/index.js
+++ final_project/index.js
@@ -11,8 +11,24 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
-});
+    const token = req.session.token;
+    if (!token) {
+        return res.status(401).json({ error: 'Unauthorized' });
+      }
+    try {
+        // Verify the token and extract the user information
+      const decoded = jwt.verify(token, 'your_secret_key'); // Replace 'your_secret_key' with your actual secret key used for JWT signing
+    
+        // Attach the user information to the request for further processing in the route handlers
+       req.user = decoded;
+        
+        // Continue to the next middleware or route handler
+        next();
+      } catch (err) {
+        // If the token is invalid, return an error response
+        return res.status(401).json({ error: 'Invalid token' });
+      }
+    });
  
 const PORT =5000;
 
